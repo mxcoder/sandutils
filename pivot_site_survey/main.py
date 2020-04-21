0609dc867d7ed19f395d0dbb5f1229cf45a49cd3
Ricardo Vega <ricardoe@gmail.com>
2020-04-21 00:20:36 -0500

    Include model length column. Modify cells conditionally to model value

--- pivot_site_survey/main.py
+++ pivot_site_survey/main.py
@@ -24,15 +24,23 @@ def main(filename):
             partial_df = original_df.loc[:, list(cols.keys())]
             partial_df.insert(1, 'Antena', antena)
             partial_df.insert(1, 'Sector', sector)
+            partial_df.insert(1, 'ModelSize', 0)
+
             partial_df = partial_df.rename(columns=cols)
             partial_df['Antena'] = f'{antena}{sector}'
+            partial_df['ModelSize'] = partial_df['Model'].apply(lambda x: len(str(x)) > 30)
             final_df = final_df.append(partial_df)
 
-    final_df.sort_values(['Customer Site ID', 'Sector', 'Antena'], inplace=True)
+    # final_df.replace(('', ' '), np.nan, inplace=True)
+    final_df['Model'].replace((0, '0', np.nan), 'NO DATA', inplace=True)
+    # final_df = final_df[final_df['Model'] == np.nan].fillna('NO DATA')
+    final_df.loc[(final_df.Model == 'NO DATA'), ('Azimut', 'Height', 'Mec. Tilt', 'Elect. Tilt')] = 'NO DATA'
+
+    final_df.replace('NO DATA', np.nan, inplace=True)
+    # final_df.sort_values(['Customer Site ID', 'Sector', 'Antena'], inplace=True)
     final_df.reset_index(drop=True, inplace=True)
-    final_df.fillna('NO DATA', inplace=True)
     # print(final_df)
-    final_df.to_csv(path.join(path.dirname(filename), f'{basefilename}_pivot.csv'))
+    final_df.to_csv(path.join(path.dirname(filename), f'{basefilename}_pivot.csv'), index=False)
 
 if __name__ == '__main__':
     filename = sys.argv.pop() if len(sys.argv) == 2 else None
